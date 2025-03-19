1f391901ccaad9ce64908f190be6e979d18b3607
Abhishek Sharma <as6119552@gmail.com>
2025-03-19 21:53:46 +0530

    Updated code and fixed build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Router,Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import VideoRoom from "./pages/VideoRoom";
 
 function App() {
   return (
+    <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/room/:roomID" element={<VideoRoom />} />
     </Routes>
+    </Router>
   );
 }
 
